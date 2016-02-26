4b793cea91b07ad6120eef4f68b486bd44e62eda
Megan Nilles <darkstarre@hotmail.com>
2016-02-25 18:04:19 -0600

    trailing commits

--- test/line_counts_test.rb
+++ test/line_counts_test.rb
@@ -1,14 +1,16 @@
 class Gitloc  
   module LineCounts
     def self.call(names_to_bodies)
-      names_to_bodies.map { |name, body|
-        begin
-          loc = body.lines.count { |line| line !~ /^\s*$/ }
+      names_to_bodies
+        .map { |name, body| [name, count_lines(body)]}
+        .select { |name, body| body}
+    end
+
+    def self.count_lines(body)
+       body.lines.count { |line| line !~ /^\s*$/ }
           [name, loc]
         rescue ArgumentError
-          # no op
+          nil
         end
-      }.compact
     end
   end
-end
